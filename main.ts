7d36d2b2d14be1ade233237aac86850339775985
Ckyiu <UnsignedArduino@outlook.com>
2021-03-03 21:58:22 -0500

    Make function to make pieces

--- main.ts
+++ main.ts
@@ -1,3 +1,41 @@
+function make_pieces () {
+    for (let location of tiles.getTilesByType(assets.tile`white_rook_tile`)) {
+    	
+    }
+    for (let location of tiles.getTilesByType(assets.tile`white_knight_tile`)) {
+    	
+    }
+    for (let location of tiles.getTilesByType(assets.tile`white_bishop_tile`)) {
+    	
+    }
+    for (let location of tiles.getTilesByType(assets.tile`white_king_tile`)) {
+    	
+    }
+    for (let location of tiles.getTilesByType(assets.tile`white_queen_tile`)) {
+    	
+    }
+    for (let location of tiles.getTilesByType(assets.tile`white_pawn_tile`)) {
+    	
+    }
+    for (let location of tiles.getTilesByType(assets.tile`black_rook_tile`)) {
+    	
+    }
+    for (let location of tiles.getTilesByType(assets.tile`black_knight_tile`)) {
+    	
+    }
+    for (let location of tiles.getTilesByType(assets.tile`black_bishop_tile`)) {
+    	
+    }
+    for (let location of tiles.getTilesByType(assets.tile`black_king_tile`)) {
+    	
+    }
+    for (let location of tiles.getTilesByType(assets.tile`black_queen_tile`)) {
+    	
+    }
+    for (let location of tiles.getTilesByType(assets.tile`black_pawn_tile`)) {
+    	
+    }
+}
 function make_cursor () {
     sprite_cursor = sprites.create(assets.image`cursor`, SpriteKind.Player)
     sprite_cursor_pointer = sprites.create(assets.image`cursor_pointer`, SpriteKind.Player)
@@ -20,6 +58,11 @@ function enable_cursor (enable: boolean) {
     } else {
         controller.moveSprite(sprite_cursor_pointer, 0, 0)
     }
+}
+function make_piece (sprite: Sprite, col: number, row: number, _type: string, color: boolean) {
+    grid.place(sprite, tiles.getTileLocation(col, row))
+    sprites.setDataString(sprite, "type", _type)
+    sprites.setDataBoolean(sprite, "color", color)
 }
 let sprite_cursor_pointer: Sprite = null
 let sprite_cursor: Sprite = null
